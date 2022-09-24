b70f7bc8209677ae826567b1b4dd57ca4c186473
Тепер Наталья Игоревна <teper@301c1.occ.local>
2022-09-24 14:30:41 +0400

    Car Model

---
+++ database/migrations/2022_09_24_081759_create_thumbnails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThumbnailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thumbnails', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('size')->nullable();
+            $table->mediumText('path')->nullable();
+            $table->string('originalname');
+            $table->string('mimetype')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thumbnails');
+    }
+}
