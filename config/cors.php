b498ce26c4ffb5abbd3534a37392fdd7ce8a9358
Natalia <teper.n@gmail.com>
2022-10-25 15:10:37 +0400

    Cors config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tepern.github.io/angular-store/order*'],
 
     'allowed_origins_patterns' => [],
 
