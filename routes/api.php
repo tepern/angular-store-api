c0450fabb9f44b40563f042fa50d34df78191dca
Natalia <teper.n@gmail.com>
2022-10-26 01:32:47 +0400

    Routes header correction

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 header('Access-Control-Allow-Origin:  https://tepern.github.io');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
 header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Credentials: true');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
