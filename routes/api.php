affb5b1c8d993e1ac4c6da17ec72c9942d649baf
Natalia <teper.n@gmail.com>
2022-10-31 21:22:25 +0400

    Cors api correction

--- routes/api.php
+++ routes/api.php
@@ -14,18 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-header('Access-Control-Allow-Origin:  https://tepern.github.io');
-header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Credentials: true');
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 $groupData = [
     'namespace' => '\App\Http\Controllers',
-    'prefix' => 'db/',
+    'prefix' => 'db',
 ];
 
 Route::group($groupData, function() {
