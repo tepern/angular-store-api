5e74348f2a6fe8d928851035da9f338d37950a93
Natalia <teper.n@gmail.com>
2022-10-31 19:15:07 +0400

    Cors settings correction

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-header('Access-Control-Allow-Origin:  *');
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
