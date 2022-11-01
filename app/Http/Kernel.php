93550adba1bb58cc6111aa44bf9b31f22db75fd8
Natalia <teper.n@gmail.com>
2022-11-01 15:13:45 +0400

    Cors setting Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,10 +17,10 @@ class Kernel extends HttpKernel
         \Fruitcake\Cors\HandleCors::class,
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        //\App\Http\Middleware\PreventRequestsDuringMaintenance::class,
-        //\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        //\App\Http\Middleware\TrimStrings::class,
-        //\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
