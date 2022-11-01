a43faacbbcf27588681a0e6819481973704d99a9
Natalia <teper.n@gmail.com>
2022-11-01 19:42:46 +0400

    Route header options

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-if (Request::getMethod() == 'OPTIONS') {
+if ($request->method() == 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
 }
 
