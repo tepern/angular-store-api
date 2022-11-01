e2df4fd422f67543f7219038e8982a3115eab245
Natalia <teper.n@gmail.com>
2022-11-01 15:31:41 +0400

    Cors headers

--- config/cors.php
+++ config/cors.php
@@ -25,7 +25,7 @@ return [
 
     'allowed_headers' => ['X-Api-Factory-Application-Id', 'Content-Type', 'Authorization', 'Accept'],
 
-    'exposed_headers' => ['*', 'Authorization', 'X-Api-Factory-Application-Id', 'Accept'],
+    'exposed_headers' => ['Authorization', 'X-Api-Factory-Application-Id', 'Accept'],
 
     'max_age' => 0,
 
