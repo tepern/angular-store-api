0603429f740e7a40db34ab2dd152947fa54fd324
Natalia <teper.n@gmail.com>
2022-12-06 21:37:14 +0400

    Render settings

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+echo "Running composer"
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
+
+echo "Caching config..."
+php artisan config:cache
+
+echo "Caching routes..."
+php artisan route:cache
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Seeding"
+php artisan migrate:fresh --seed
+
+echo "Admin panel"
+php artisan vendor:publish --provider="Encore\Admin\AdminServiceProvider"
+
+echo "Storage Public"
+php artisan storage:link
+
+echo "Storage Images"
+php artisan storage:link
